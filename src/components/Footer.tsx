4bd3a9ea31fee4654348777d1169a2560b659b96
Ahmad jamil <shazabjamildhami@gmail.com>
2025-06-09 15:17:29 +0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -102,7 +102,7 @@ const Footer: React.FC = () => {
         {/* Bottom Bar */}
         <div className="border-t border-gray-800 mt-8 pt-8 flex flex-col md:flex-row justify-between items-center">
           <p className="text-gray-400 text-sm">
-            © 2025 E-cart. All rights reserved.
+            © 2025 E-cart. All rights reserved by <a href="https://aurionsoft.site/">Aurion soft</a>.
           </p>
           <div className="flex space-x-6 mt-4 md:mt-0">
             <a href="#" className="text-gray-400 hover:text-white text-sm transition-colors">
